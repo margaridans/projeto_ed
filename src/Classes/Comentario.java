10757f0dec063ad491c89a469493e70e438bbd30
Marisa <marisa_machado95@hotmail.com>
2018-02-03 22:36:51 +0000

    base dados criada + classes

--- src/Classes/Comentario.java
+++ src/Classes/Comentario.java
@@ -16,7 +16,67 @@ public class Comentario {
     private int id_comentario;
     private String comentario;
     private Date data_comentario; //Data de quando foi feito o comentário
-    private Pessoa nome_remetente; // Nome de quem escreveu o comentário
+    private Pessoa email_user; // Email de quem escreveu o comentário
+    private Mensagem id_mensagem; // Id da mensagem associada ao comentário
+
+    /*Método construtor*/
+    public Comentario(int id_comentario, String comentario, Date data_comentario, Pessoa email_user, Mensagem id_mensagem) {
+        this.id_comentario = id_comentario;
+        this.comentario = comentario;
+        this.data_comentario = data_comentario;
+        this.email_user = email_user;
+        this.id_mensagem = id_mensagem;
+    }
+
+    /*Método construtor vazio*/
+    public Comentario() {
+    }
+
+    
+    
+    
+    
+    public int getId_comentario() {
+        return id_comentario;
+    }
+
+    public void setId_comentario(int id_comentario) {
+        this.id_comentario = id_comentario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Date getData_comentario() {
+        return data_comentario;
+    }
+
+    public void setData_comentario(Date data_comentario) {
+        this.data_comentario = data_comentario;
+    }
+
+    public Pessoa getEmail_user() {
+        return email_user;
+    }
+
+    public void setEmail_user(Pessoa email_user) {
+        this.email_user = email_user;
+    }
+
+    public Mensagem getId_mensagem() {
+        return id_mensagem;
+    }
+
+    public void setId_mensagem(Mensagem id_mensagem) {
+        this.id_mensagem = id_mensagem;
+    }
+    
+    
     
 
 
