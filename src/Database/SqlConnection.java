752ef7572a22cc25f56babd22f1dc7a8b6405890
Marisa <marisa_machado95@hotmail.com>
2018-02-03 22:15:57 +0000

    base dados criada + classes

--- src/Database/SqlConnection.java
+++ src/Database/SqlConnection.java
@@ -9,15 +9,16 @@ import java.sql.Statement;
 import org.sqlite.SQLiteConfig;
 
 /**
- *
  * @author Marisa Machado - 8140
- * @author Margarida Sousa - 8140
  */
 public class SqlConnection {
 
     private final String DRIVER = "org.sqlite.JDBC";
-    private final String TABLE_PERSON = "Person";
-    private final String TABLE_MESSAGE = "Message";
+    private final String TABELA_PESSOA = "Pessoa";
+    private final String TABELA_COMENTARIO = "Comentario";
+    private final String TABELA_MENSAGEM = "Messagem";
+    private final String TABELA_TIPO_MENSAGEM = "TipoMensagem";
+    private final String TABELA_AMIZADE = "Amizade";
     private final String SQL_LITE = "jdbc:sqlite:";
     private String DB_NAME = "basedadosed.db";
     private Connection connection = null;
@@ -30,48 +31,46 @@ public class SqlConnection {
     public SqlConnection() {
         try {
             Class.forName(DRIVER);
-            SQLiteConfig config = new SQLiteConfig();  
-           config.enforceForeignKeys(true);  
-            connection = DriverManager.getConnection(SQL_LITE + DB_NAME,config.toProperties());
+            SQLiteConfig config = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+            connection = DriverManager.getConnection(SQL_LITE + DB_NAME, config.toProperties());
             DatabaseMetaData dmd = this.connection.getMetaData();
-            ResultSet tables = dmd.getTables(null, null, "Person", null);
-            if(!tables.next()){
-                createPersonTable();
-                createMessageTable();
+            ResultSet tables = dmd.getTables(null, null, "Pessoa", null);
+            if (!tables.next()) {
+                criarTabelaPessoa();
+                criarTabelaMensagem();
+                criarTabelaComentario();
+                criarTabelaTipoMensagem();
+                criarTabelaAmizade();
             }
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
     }
-    
-    
-   /**
-    * Criar a tabela que armazena Pessoas
-    * 
-    */ 
-    
-    public void createPersonTable(){
+
+    /*Criação da tabela que armazena pessoas*/
+    public void criarTabelaPessoa() {
         Statement stm = null;
         try {
             stm = connection.createStatement();
-            String sqlTable = "CREATE TABLE " + TABLE_PERSON
+            String sqlTable = "CREATE TABLE " + TABELA_PESSOA
                     + " (EMAIL VARCHAR(50) PRIMARY KEY NOT NULL,"
-                    + "NAME VARCHAR(50) NOT NULL,"
+                    + "NOME_USER VARCHAR(50) NOT NULL,"
                     + "PASSWORD VARCHAR(50) NOT NULL)";
 
             stm.executeUpdate(sqlTable);
         } catch (SQLException ex) {
-            System.out.println("A tabela Person já existe!!");
+            System.out.println("A tabela 'Pessoa' já existe!");
         }
     }
-    
-    
-    public void createMessageTable(){
-         Statement stm = null;
+
+    /*Criação da tabela que armazena mensagens*/
+    public void criarTabelaMensagem() {
+        Statement stm = null;
         try {
             stm = connection.createStatement();
             stm.execute("PRAGMA foreign_keys = ON");
-            String sqlTable = "CREATE TABLE " + TABLE_MESSAGE
+            String sqlTable = "CREATE TABLE " + TABELA_MENSAGEM
                     + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,"
                     + "CONTEUDO VARCHAR(100) NOT NULL,"
                     + "DATA TEXT NOT NULL,"
@@ -80,31 +79,66 @@ public class SqlConnection {
 
             stm.executeUpdate(sqlTable);
         } catch (SQLException ex) {
-            System.out.println(ex);
+            System.out.println("A tabela 'Mensagem' já existe!");
         }
     }
-    
-    
 
-//    /**
-//     * Criar a tabela de Edges
-//     */
-//    public void createTableEdge() {
-//        Statement statement = null;
-//        try {
-//            statement = connection.createStatement();
-//            String sqlTable = "CREATE TABLE " + TABLE_NAME_EDGES
-//                    + " (ID INT PRIMARY KEY NOT NULL,"
-//                    + "ORIGIN VARCHAR(50) NOT NULL,"
-//                    + "DESTINATION VARCHAR(50) NOT NULL,"
-//                    + "TIME FLOAT NOT NULL,"
-//                    + "DISTANCE FLOAT NOT NULL)";
-//
-//            statement.executeUpdate(sqlTable);
-//        } catch (SQLException ex) {
-//            this.tableEdgesExists = true;
-//        }
-//    }
+    /*Criação da tabela que armazena comentário*/
+    public void criarTabelaComentario() {
+        Statement stm = null;
+        try {
+            stm = connection.createStatement();
+            stm.execute("PRAGMA foreign_keys = ON");
+            String sqlTable = "CREATE TABLE " + TABELA_COMENTARIO
+                    + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "CONTEUDO VARCHAR(100) NOT NULL,"
+                    + "DATA TEXT NOT NULL,"
+                    + "USER_EMAIL VARCHAR(50) NOT NULL,"
+                    + "FOREIGN KEY(USER_EMAIL) REFERENCES Person(EMAIL))";
+
+            stm.executeUpdate(sqlTable);
+        } catch (SQLException ex) {
+            System.out.println("A tabela 'Comentário' já existe!");
+        }
+    }
+
+    /*Criação da tabela que armazena os tipos de mensagens*/
+    public void criarTabelaTipoMensagem() {
+        Statement stm = null;
+        try {
+            stm = connection.createStatement();
+            stm.execute("PRAGMA foreign_keys = ON");
+            String sqlTable = "CREATE TABLE " + TABELA_TIPO_MENSAGEM
+                    + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "CONTEUDO VARCHAR(100) NOT NULL,"
+                    + "DATA TEXT NOT NULL,"
+                    + "USER_EMAIL VARCHAR(50) NOT NULL,"
+                    + "FOREIGN KEY(USER_EMAIL) REFERENCES Person(EMAIL))";
+
+            stm.executeUpdate(sqlTable);
+        } catch (SQLException ex) {
+            System.out.println("A tabela 'Tipo Mensagem' já existe!");
+        }
+    }
+
+    public void criarTabelaAmizade() {
+        Statement stm = null;
+        try {
+            stm = connection.createStatement();
+            stm.execute("PRAGMA foreign_keys = ON");
+            String sqlTable = "CREATE TABLE " + TABELA_AMIZADE
+                    + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "CONTEUDO VARCHAR(100) NOT NULL,"
+                    + "DATA TEXT NOT NULL,"
+                    + "USER_EMAIL VARCHAR(50) NOT NULL,"
+                    + "FOREIGN KEY(USER_EMAIL) REFERENCES Person(EMAIL))";
+
+            stm.executeUpdate(sqlTable);
+        } catch (SQLException ex) {
+            System.out.println("A tabela 'Amizade' já existe!");
+        }
+    }
+
 //
 //    /**
 //     * Inserir edges na tabela de Edges
@@ -227,7 +261,6 @@ public class SqlConnection {
 //        }
 //        return num;
 //    }
-
     /**
      * Fechar a connexão à base de dados
      */
@@ -238,6 +271,5 @@ public class SqlConnection {
             System.err.print(SqlConnection.class.getName() + ": " + ex.getMessage());
         }
     }
-
 
 }
