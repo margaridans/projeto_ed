ffaaf3e607f290c329ddd07a2cc7727ee077316d
Marisa <marisa_machado95@hotmail.com>
2018-02-14 01:07:24 +0000

    mais

---
+++ src/Classes/Pedido.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Bernardino
+ */
+public class Pedido {
+
+    private Integer id_pedidoAmizade;
+    private Pessoa user_origem;
+    private Pessoa user_destino;
+    private Integer id_estado;
+
+    public Pedido(Pessoa user_origem, Pessoa user_destino, Integer id_estado) {
+        this.user_origem = user_origem;
+        this.user_destino = user_destino;
+        this.id_estado = id_estado;
+    }
+
+    public Pedido() {
+    }
+
+   
+
+    public Integer getId_pedidoAmizade() {
+        return id_pedidoAmizade;
+    }
+
+    public void setId_pedidoAmizade(Integer id_pedidoAmizade) {
+        this.id_pedidoAmizade = id_pedidoAmizade;
+    }
+
+    public Pessoa getUser_origem() {
+        return user_origem;
+    }
+
+    public void setUser_origem(Pessoa user_origem) {
+        this.user_origem = user_origem;
+    }
+
+    public Pessoa getUser_destino() {
+        return user_destino;
+    }
+
+    public void setUser_destino(Pessoa user_destino) {
+        this.user_destino = user_destino;
+    }
+
+}
